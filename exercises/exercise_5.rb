3ad547c3ba659d509310db9d9382d8a22e4332fa
Kurt Maglalang <kbm126@live.com>
2022-05-02 22:26:22 -0700

    implement store sum averge annual revenue

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -4,7 +4,16 @@ require_relative './exercise_2'
 require_relative './exercise_3'
 require_relative './exercise_4'
 
-puts "Exercise 5"
-puts "----------"
+# Exercise 5: Calculations
+# Output the total revenue for the entire company (all stores), using Active Record's .sum calculation method.
+# On the next line, also output the average annual revenue for all stores.
+# Output the number of stores that are generating $1M or more in annual sales. Hint: Chain together where and size (or count) Active Record methods.
 
-# Your code goes here ...
+puts 'Exercise 5'
+puts '----------'
+
+puts 'sum', Store.sum(:annual_revenue)
+puts 'average', Store.average(:annual_revenue)
+
+puts 'number of stores that are generating $1M or more in annual sales'
+puts Store.where('annual_revenue > 1000000').count
