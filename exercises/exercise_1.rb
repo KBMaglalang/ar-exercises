debc2b06efd03cbdbe638877aeec26b5ea88c17e
Kurt Maglalang <kbm126@live.com>
2022-05-02 22:27:36 -0700

    @ added to store name

--- exercises/exercise_1.rb
+++ exercises/exercise_1.rb
@@ -9,8 +9,8 @@ require_relative '../setup'
 
 puts 'Exercise 1'
 puts '----------'
-store1 = Store.create({ name: 'Burnaby', annual_revenue: 300_000, mens_apparel: true, womens_apparel: true })
-store2 = Store.create({ name: 'Richmond', annual_revenue: 1_260_000, mens_apparel: false, womens_apparel: true })
-store3 = Store.create({ name: 'Gastown', annual_revenue: 190_000, mens_apparel: true, womens_apparel: false })
+@store1 = Store.create({ name: 'Burnaby', annual_revenue: 300_000, mens_apparel: true, womens_apparel: true })
+@store2 = Store.create({ name: 'Richmond', annual_revenue: 1_260_000, mens_apparel: false, womens_apparel: true })
+@store3 = Store.create({ name: 'Gastown', annual_revenue: 190_000, mens_apparel: true, womens_apparel: false })
 
 puts "Number of Stores: #{Store.count}"
